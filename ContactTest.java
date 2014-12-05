6bb62a288c4ddf21070e0b71a84abf873aa809d4
BBK-PiJ-2014-14 <skbigmac@hotmail.com>
2014-12-05 21:15:32 +0000

    Add of Test classes
    Test classes added without implementation

---
+++ ContactTest.java
@@ -0,0 +1,30 @@
+package Cw4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactTest {
+
+	@Test
+	public void testGetId() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNotes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNotes() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ FutureMeetingTest.java
@@ -0,0 +1,24 @@
+package Cw4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FutureMeetingTest {
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContacts() {
+		fail("Not yet implemented");
+	}
+
+}
