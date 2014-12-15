cf5545cb29dd2407b2e5169c2728b94383cae473
BBK-PiJ-2014-14 <skbigmac@hotmail.com>
2014-12-15 16:56:17 +0000

    Implementations added to Contact.
    Implementations of getNotes() and addNotes() added.
    all tests passed.
    A constructor of the list was added to the constructor method.

--- ContactImp.java
+++ ContactImp.java
@@ -1,11 +1,13 @@
 package Cw4;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class ContactImp implements Contact {
 	private String name;
 	private int iD;
-	private List<String> note;
+	private List<String> notes;
 	private static int contactNum = 1;
 	
 	
@@ -14,6 +16,7 @@ public class ContactImp implements Contact {
 		this.name = name;
 		this.iD = contactNum;
 		ContactImp.contactNum++;
+		this.notes = new ArrayList<String>();
 	}
 
 	// this method return the ID of the contact.
@@ -31,14 +34,20 @@ public class ContactImp implements Contact {
 
 	@Override
 	public String getNotes() {
-		// TODO Auto-generated method stub
-		return null;
+		String result = "";
+		if (!notes.isEmpty()) {
+			for (int i = 0; i < notes.size(); i++) {
+				result = result + "[" + notes.get(i) + "]";
+			}
+		}
+		return result;
+
 	}
 
+	// This method add a note about the contact.
 	@Override
 	public void addNotes(String note) {
-		// TODO Auto-generated method stub
-
+		notes.add(note);
 	}
 
 }
