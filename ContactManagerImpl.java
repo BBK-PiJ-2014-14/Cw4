0aac2a5687915502645c1b8f85ec415582d792cd
BBK-PiJ-2014-14 <skbigmac@hotmail.com>
2015-01-02 15:25:22 +0000

    First implementation of ContactManagerImpl
    Some methods passed the test and some are not done.

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -2,97 +2,207 @@ package Cw4;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ContactManagerImpl implements ContactManager {
-	private List<FutureMeetingImpl> future;
-	private List<PastMeetingImpl> past;
-	private List<ContactImpl> contacts;
+	private List<FutureMeeting> future;
+	private List<PastMeeting> past;
+	private List<Contact> contacts;
 	
 	
 	/**
 	 * Constructor
 	 */
 	public ContactManagerImpl() {
-		future = new ArrayList<FutureMeetingImpl>();
-		past = new ArrayList<PastMeetingImpl>();
-		contacts = new ArrayList<ContactImpl>();	
+		future = new ArrayList<FutureMeeting>();
+		past = new ArrayList<PastMeeting>();
+		contacts = new ArrayList<Contact>();	
 	}
 	
 
 	@Override
-	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
-		Meeting newMeeting = new FutureMeetingImpl(date, contacts);
-		// TODO Auto-generated method stub
-		return 0;
+	public int addFutureMeeting(Set<Contact> contacts, Calendar date)throws IllegalArgumentException {
+		Calendar now =Calendar.getInstance();
+		if (date.before(now)|| !this.contacts.containsAll(contacts)) {
+			throw new IllegalArgumentException();
+		}
+		FutureMeeting newMeeting = new FutureMeetingImpl(date, contacts);
+		future.add(newMeeting);
+		return newMeeting.getId();
 	}
 
 	@Override
 	public PastMeeting getPastMeeting(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		PastMeeting result = null;
+		for(int i=0; i<past.size(); i++) {
+			if(past.get(i).getId()==id){
+				result = past.get(i);
+				break;
+			}	
+		}
+		return result;
 	}
 
 	@Override
 	public FutureMeeting getFutureMeeting(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		FutureMeeting result = null;
+		for(int i=0; i<future.size(); i++) {
+			if(future.get(i).getId()==id) {
+				result = future.get(i);
+				break;
+			}
+		}
+
+		return result;
 	}
 
 	@Override
 	public Meeting getMeeting(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Meeting result = getFutureMeeting(id);
+		if (result == null) {
+			result = getPastMeeting(id);
+		}
+		return result;
 	}
 
 	@Override
 	public List<Meeting> getFutureMeetingList(Contact contact) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!contacts.contains(contact)) {
+			throw new IllegalArgumentException();
+		}
+		List<Meeting> result= new ArrayList<Meeting>();
+		for(int i=0; i<future.size(); i++) {
+			if (future.get(i).getContacts().contains(contact)) {
+				result.add(future.get(i));
+			}
+		}
+		
+		
+		//Collections.sort(result);
+		return result;
 	}
 
 	@Override
 	public List<Meeting> getFutureMeetingList(Calendar date) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Meeting> result = new ArrayList<Meeting>();
+		for(int i=0; i<future.size(); i++) {
+			if (future.get(i).getDate() == date) {
+				result.add(future.get(i));
+			}
+		}
+		return result;
 	}
 
 	@Override
-	public List<PastMeeting> getPastMeetingList(Contact contact) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<PastMeeting> getPastMeetingList(Contact contact) throws IllegalArgumentException{
+		if (!this.contacts.contains(contact)) {
+			throw new IllegalArgumentException();		
+		}
+		List<PastMeeting> result = new ArrayList<PastMeeting>();
+		for (int i=0; i<past.size(); i++) {
+			if (past.get(i).getContacts().contains(contact)) {
+				result.add(past.get(i));
+			}
+		}
+		
+		return result;
 	}
 
 	@Override
-	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
-			String text) {
-		// TODO Auto-generated method stub
-
+	public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) 
+			throws IllegalArgumentException, NullPointerException {
+		if(contacts == null || date == null || text ==null) {
+			throw new NullPointerException();
+		}
+		if(contacts.isEmpty() || !this.contacts.containsAll(contacts)) {
+			throw new IllegalArgumentException();
+		}
+		PastMeetingImpl newPast = new PastMeetingImpl(date, contacts);
+		newPast.addNotes(text);
+		past.add(newPast);
 	}
 
 	@Override
-	public void addMeetingNotes(int id, String text) {
-		// TODO Auto-generated method stub
-
+	public void addMeetingNotes(int id, String text) 
+			throws IllegalArgumentException, IllegalStateException, NullPointerException {
+		if (getMeeting(id)==null) {
+			throw new IllegalArgumentException();
+		}
+		if (text == null) {
+			throw new NullPointerException();
+		}
+		Calendar now = Calendar.getInstance();
+		if (getMeeting(id).getDate().after(now)) {
+			throw new IllegalStateException();
+		}
+		if (getPastMeeting(id)!=null) {
+			PastMeetingImpl newPast = (PastMeetingImpl)getPastMeeting(id);
+			newPast.addNotes(text);
+			for(int i=0; i<past.size(); i++) {
+				if(getPastMeeting(id)==past.get(i)) {
+					past.remove(i);
+					break;
+				}
+			}
+			past.add(newPast);
+		} else {
+			Set<Contact> con = getFutureMeeting(id).getContacts();
+			Calendar date = getFutureMeeting(id).getDate();
+			PastMeetingImpl newPast = new PastMeetingImpl(date, con);
+			newPast.addNotes(text);
+			past.add(newPast);
+			for(int i=0; i<future.size(); i++) {
+				if(getFutureMeeting(id)==future.get(i)) {
+					future.remove(i);
+					break;
+				}
+			}
+		}
 	}
 
 	@Override
-	public void addNewContact(String name, String notes) {
-		// TODO Auto-generated method stub
-
+	public void addNewContact(String name, String notes) throws NullPointerException {
+		if(name == null || notes == null) {
+			throw new NullPointerException();
+		}
+		ContactImpl newCont = new ContactImpl(name);
+		newCont.addNotes(notes);
+		contacts.add(newCont);
 	}
 
 	@Override
 	public Set<Contact> getContacts(int... ids) {
-		// TODO Auto-generated method stub
-		return null;
+		Set<Contact> result = new HashSet<Contact>();
+		for(int n=0; n<ids.length; n++){
+			for (int i=0; i<contacts.size(); i++) {
+				if(contacts.get(i).getId() == ids[n]) {
+					result.add(contacts.get(i));
+					
+				} 
+			}   
+		}
+		if (result.size() != ids.length ) {
+			throw new IllegalArgumentException();
+		}
+		
+		return result;
 	}
 
 	@Override
-	public Set<Contact> getContacts(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<Contact> getContacts(String name) throws NullPointerException {
+		if (name == null) {
+			throw new NullPointerException();
+		}
+		Set<Contact> result = new HashSet<Contact>();
+		for(int n=0; n<contacts.size();n++) {
+			if(contacts.get(n).getName() == name) {
+				result.add(contacts.get(n));
+			}	
+		}
+		return result;
 	}
 
 	@Override
