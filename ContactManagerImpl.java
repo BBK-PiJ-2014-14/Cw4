efb7d4bad41029e46456adeeccff6997f0d19fc1
BBK-PiJ-2014-14 <skbigmac@hotmail.com>
2015-01-02 20:16:12 +0000

    Implementations complete
    All implementations to existing tests of ContactManagerImpl were
    complete and passed.
    Still one methods left to be implemented after the test will be ready.

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -165,10 +165,21 @@ public class ContactManagerImpl implements ContactManager {
 		return result;	
 	}
 
+	/**
+	* Returns the list of past meetings in which this contact has participated.
+	*
+	* If there are none, the returned list will be empty. Otherwise,
+	* the list will be chronologically sorted and will not contain any
+	2* duplicates.
+	*
+	* @param contact one of the user’s contacts
+	* @return the list of future meeting(s) scheduled with this contact (maybe empty).
+	* @throws IllegalArgumentException if the contact does not exist
+	*/
 	@Override
 	public List<PastMeeting> getPastMeetingList(Contact contact) throws IllegalArgumentException{
 		if (!this.contacts.contains(contact)) {
-			throw new IllegalArgumentException();		
+			throw new IllegalArgumentException("Contact does not exist");		
 		}
 		List<PastMeeting> result = new ArrayList<PastMeeting>();
 		for (int i=0; i<past.size(); i++) {
@@ -176,35 +187,60 @@ public class ContactManagerImpl implements ContactManager {
 				result.add(past.get(i));
 			}
 		}
+		Collections.sort(result, dateComp);
 		return result;
 	}
 
+	/**
+	* Create a new record for a meeting that took place in the past.
+	*
+	* @param contacts a list of participants
+	* @param date the date on which the meeting took place
+	* @param text messages to be added about the meeting.
+	* @throws IllegalArgumentException if the list of contacts is
+	* empty, or any of the contacts does not exist
+	* @throws NullPointerException if any of the arguments is null
+	*/
 	@Override
 	public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) 
 			throws IllegalArgumentException, NullPointerException {
-		if(contacts == null || date == null || text ==null) {
-			throw new NullPointerException();
+		if(contacts == null || date == null || text == null) {
+			throw new NullPointerException("Missing arguments");
 		}
 		if(contacts.isEmpty() || !this.contacts.containsAll(contacts)) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Contact not found");
 		}
 		PastMeetingImpl newPast = new PastMeetingImpl(date, contacts);
 		newPast.addNotes(text);
 		past.add(newPast);
 	}
 
+	/**
+	* Add notes to a meeting.
+	*
+	* This method is used when a future meeting takes place, and is
+	* then converted to a past meeting (with notes).
+	*
+	* It can be also used to add notes to a past meeting at a later date.
+	*
+	* @param id the ID of the meeting
+	* @param text messages to be added about the meeting.
+	* @throws IllegalArgumentException if the meeting does not exist
+	* @throws IllegalStateException if the meeting is set for a date in the future
+	* @throws NullPointerException if the notes are null
+	*/
 	@Override
 	public void addMeetingNotes(int id, String text) 
 			throws IllegalArgumentException, IllegalStateException, NullPointerException {
 		if (getMeeting(id)==null) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Meeting does not exists");
 		}
 		if (text == null) {
-			throw new NullPointerException();
+			throw new NullPointerException("No notes added");
 		}
 		Calendar now = Calendar.getInstance();
 		if (getMeeting(id).getDate().after(now)) {
-			throw new IllegalStateException();
+			throw new IllegalStateException("Date is not in the past");
 		}
 		if (getPastMeeting(id)!=null) {
 			PastMeetingImpl newPast = (PastMeetingImpl)getPastMeeting(id);
@@ -231,16 +267,30 @@ public class ContactManagerImpl implements ContactManager {
 		}
 	}
 
+	/**
+	* Create a new contact with the specified name and notes.
+	*
+	* @param name the name of the contact.
+	* @param notes notes to be added about the contact.
+	* @throws NullPointerException if the name or the notes are null
+	*/
 	@Override
 	public void addNewContact(String name, String notes) throws NullPointerException {
 		if(name == null || notes == null) {
-			throw new NullPointerException();
+			throw new NullPointerException("Missing argument");
 		}
 		ContactImpl newCont = new ContactImpl(name);
 		newCont.addNotes(notes);
 		contacts.add(newCont);
 	}
 
+	/**
+	* Returns a list containing the contacts that correspond to the IDs.
+	*
+	* @param ids an arbitrary number of contact IDs
+	* @return a list containing the contacts that correspond to the IDs.
+	* @throws IllegalArgumentException if any of the IDs does not correspond to a real contact
+	*/
 	@Override
 	public Set<Contact> getContacts(int... ids) {
 		Set<Contact> result = new HashSet<Contact>();
@@ -253,16 +303,23 @@ public class ContactManagerImpl implements ContactManager {
 			}   
 		}
 		if (result.size() != ids.length ) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("wrong id, Can't find contact");
 		}
 		
 		return result;
 	}
 
+	/**
+	* Returns a list with the contacts whose name contains that string.
+	3*
+	* @param name the string to search for
+	* @return a list with the contacts whose name contains that string.
+	* @throws NullPointerException if the parameter is null
+	*/
 	@Override
 	public Set<Contact> getContacts(String name) throws NullPointerException {
 		if (name == null) {
-			throw new NullPointerException();
+			throw new NullPointerException("Name not initialized");
 		}
 		Set<Contact> result = new HashSet<Contact>();
 		for(int n=0; n<contacts.size();n++) {
